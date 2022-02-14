041b8c11a257de1d361431f4c33aacfbe78ff254
Sy Rashid <sy.rashid.11@gmail.com>
2022-02-14 11:31:31 +0000

    FEAT: Add members to about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,13 @@ class PagesController < ApplicationController
   def home
     # Some sort of model interaction potentially
     # Load some variables
+    raise
+    @members = ['donnie', 'sally', 'nico', 'sheila', 'bobbo']
+
+    if params[:member]
+      @members = @members.select do |member|
+        member.start_with?(params["member"].downcase)
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # Verb 'path', to: 'controller#action'
   root to: 'pages#home'
 
-  get 'about', to: 'pages#about'
+  get 'about', to: 'pages#about', as: :banana
 end
